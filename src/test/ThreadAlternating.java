62bf129acdc7b26508ec94006d93078f58421e36
Amit Mohanty <mohanty.am22@gmail.com>
2021-10-08 14:55:44 +0530

    Two Threads Alternating to Print 10 Numbers Sequentilly

--- src/test/ThreadAlternating.java
+++ src/test/ThreadAlternating.java
@@ -1,10 +1,69 @@
 package test;
 
 public class ThreadAlternating {
+	
+	int counter = 1;
+	
+	static int N;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		N = 10;
+		
+		ThreadAlternating ta = new ThreadAlternating();
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ta.printEven();
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ta.printOdd();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+	}
 
+	public void printOdd() {
+		synchronized (this) {
+			while (counter < N) {
+				while (counter % 2 == 0 ) {
+					try {
+						wait();
+					}
+					catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.print(counter + " ");
+				counter ++;
+				notify();
+			}
+		}
+	}
+
+	public void printEven() {
+		synchronized (this) {
+			while (counter < N) {
+				while (counter % 2 != 0) {
+					try {
+						wait();
+					}
+					catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.print(counter + " ");
+				counter ++;
+				notify();
+			}
+		}
 	}
 
 }
